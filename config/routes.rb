9d9f5e48388cece93c25d187215a0888e37dd737
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-14 00:39:54 +0000

    ページにマップを表示できた

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'maps/index'
   root :to =>"homes#top"
+  resources :maps
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
